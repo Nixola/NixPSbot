21992b33830fc1996bd12f193c9b9112732b4732
Nix <niki.or97@yahoo.it>
2016-08-02 20:14:17 +0200

    Can log to stdout

--- log.lua
+++ log.lua
@@ -1,4 +1,9 @@
-local f = io.open("/tmp/nix.showbot.log", "w")
+local f
+if cmdline.log == "stdout" then
+  f = io.stdout
+else
+  f = io.open("/tmp/nix.showbot.log", "w")
+end
 
 local log = function(msg)
   f:write(msg)
