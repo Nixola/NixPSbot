58ee84ea48f377f707311df13dc4b0efd51bc362
Nix <niki.or97@yahoo.it>
2016-08-03 13:39:42 +0200

    Added public url encoding string method

---
+++ urlencode.lua
@@ -0,0 +1,9 @@
+string.encode = function(str)
+  if (str) then
+    str = string.gsub (str, "\n", "\r\n")
+    str = string.gsub (str, "([^%w %-%_%.%~])",
+        function (c) return string.format ("%%%02X", string.byte(c)) end)
+    str = string.gsub (str, " ", "+")
+  end
+  return str	
+end
