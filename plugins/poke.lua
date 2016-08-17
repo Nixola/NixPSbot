cd4e80bbd61aa052914b965e3cf1a9baa0a53175
Nix <niki.or97@yahoo.it>
2016-08-17 18:24:05 +0200

    Poke the bot 2.0

--- plugins/poke.lua
+++ plugins/poke.lua
@@ -3,7 +3,7 @@ local poke = function(rest, room)
   rest = rest:match("^%d+|(.-)$")
   local nick, msg = rest:match("^(.-)|(.+)$")
   local target = msg:match("^%/me pokes (.-)$")
-  if target and target:trueNick() == cmdline.trueNick() then
+  if target and target:trueNick() == cmdline.nick:trueNick() then
   	if math.random() <= 0.1 then
   	  send(room, "|/me pokes " .. nick .. " back.")
   	else
